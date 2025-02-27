745f2669580db1432c5b55cd8b47da744a3fd66f
Sorin Constantinescu <kn6yuh@gmail.com>
2025-02-26 22:48:23 -0800

    adding simple go.mod and empty server_test.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PAARA-org/compass
+
+go 1.21.0
---
+++ server_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	// TODO(kn6yuh): Add  test cases here
+}
