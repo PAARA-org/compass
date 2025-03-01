1cc98dcc7662c4362cd2179e26925bfe9b741d40
Sorin Constantinescu <kn6yuh@gmail.com>
2025-02-28 20:53:42 -0800

    adding copyright header to the new files

--- web_templates.go
+++ web_templates.go
@@ -1,3 +1,10 @@
+/* Copyright (c) 2025, kn6yuh@gmail.com, PAARA.org
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 package main
 
 const pageHeader = `<!DOCTYPE html>
